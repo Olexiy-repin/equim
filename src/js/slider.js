af78916e6c312a511e5ab140681fd58a4849556d
Olexiy repin <repinlyoha@gmail.com>
2018-07-23 01:31:04 +0300

    add 3 sections for mobile

--- src/js/slider.js
+++ src/js/slider.js
@@ -27,3 +27,18 @@
     margin : 5
   });
 })();
+
+(() => {
+  $(".sec-10__slider").owlCarousel({
+   items : 1,
+   itemsCustom : true,
+
+   loop: true,
+   nav: false,
+   dot: true,
+
+    autoHeight : false,
+
+    margin : 5
+  });
+})();
